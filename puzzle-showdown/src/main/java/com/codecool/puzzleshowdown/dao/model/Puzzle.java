c83b795548c934c253c6daa4797624d2381dbf04
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-02-13 14:23:30 +0100

    Implement login

--- puzzle-showdown/src/main/java/com/codecool/puzzleshowdown/dao/model/Puzzle.java
+++
@@ -1,4 +0,0 @@
-package com.codecool.puzzleshowdown.dao.model;
-
-public record Puzzle(String puzzleId, String fen, String moves, int rating, int ratingDeviation, int popularity, int nbPlays, String themes, String gameUrl) {
-}
