a474b3f1b53f5d8fd9b57bc6da39dae548a135a5
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-02-13 13:45:10 +0100

    Create user email validator

--- puzzle-showdown/src/main/java/com/codecool/puzzleshowdown/service/UserService.java
+++ puzzle-showdown/src/main/java/com/codecool/puzzleshowdown/service/UserService.java
@@ -1,15 +1,21 @@
 package com.codecool.puzzleshowdown.service;
 
 import com.codecool.puzzleshowdown.custom_exception.AlreadyExistingUserException;
+import com.codecool.puzzleshowdown.dto.user.UserLoginDTO;
 import com.codecool.puzzleshowdown.dto.user.UserRegistrationDTO;
 import com.codecool.puzzleshowdown.model.user.User;
 import com.codecool.puzzleshowdown.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.regex.Pattern;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
 
+    @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
@@ -28,6 +34,32 @@ public class UserService {
         } catch (Exception e){
             throw new AlreadyExistingUserException(userRegistration.userName());
         }
+    }
 
+    public boolean userValidation(UserLoginDTO userLoginDTO){
+        try{
+            Optional<User> optionalUser;
+            boolean isEmail = emailValidator(userLoginDTO.authenticator());
+
+            getUserInfo(userLoginDTO, isEmail);
+            return true;
+
+        } catch (Exception e){
+            throw new RuntimeException();
+        }
+    }
+
+    private void getUserInfo(UserLoginDTO userLoginDTO, boolean isEmail) {
+        Optional<User> optionalUser;
+        if(isEmail){
+            optionalUser = userRepository.findByEmail(userLoginDTO.authenticator());
+        } else {
+            optionalUser = userRepository.findByUserName(userLoginDTO.authenticator());
+        }
+    }
+
+    private boolean emailValidator(String userAuthenticator){
+        String emailRegex = "^(.+)@(\\S+) $.";
+        return Pattern.compile(emailRegex).matcher(userAuthenticator).matches();
     }
 }
