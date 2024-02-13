e3effbb957ed3a31ac4ecc331d50ab04d218ac00
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-02-13 11:34:16 +0100

    Create User Repository

---
+++ puzzle-showdown/src/main/java/com/codecool/puzzleshowdown/controller/UserController.java
@@ -0,0 +1,2 @@
+package com.codecool.puzzleshowdown.controller;public class UserController {
+}
---
+++ puzzle-showdown/src/main/java/com/codecool/puzzleshowdown/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.codecool.puzzleshowdown.repository;
+
+import com.codecool.puzzleshowdown.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUserName(String userName);
+}
