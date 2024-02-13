c5567f787f1b1c45a09f02d4180e97dca94cdcd0
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-02-13 12:08:54 +0100

    Implement saveUser method in UserRepository

--- puzzle-showdown/src/main/java/com/codecool/puzzleshowdown/service/UserService.java
+++ puzzle-showdown/src/main/java/com/codecool/puzzleshowdown/service/UserService.java
@@ -1,2 +1,33 @@
-package com.codecool.puzzleshowdown.service;public class UserService {
+package com.codecool.puzzleshowdown.service;
+
+import com.codecool.puzzleshowdown.custom_exception.AlreadyExistingUserException;
+import com.codecool.puzzleshowdown.dto.user.UserRegistrationDTO;
+import com.codecool.puzzleshowdown.model.user.User;
+import com.codecool.puzzleshowdown.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    private User saveUser(UserRegistrationDTO userRegistration){
+        try{
+            User user = new User(
+                    userRegistration.firstName(),
+                    userRegistration.lastName(),
+                    userRegistration.userName(),
+                    userRegistration.email(),
+                    userRegistration.password()
+            );
+
+            return userRepository.save(user);
+        } catch (Exception e){
+            throw new AlreadyExistingUserException(userRegistration.userName());
+        }
+
+    }
 }
