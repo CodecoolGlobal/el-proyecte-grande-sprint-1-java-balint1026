c01dd34242e80f19f9ea132c6e85e2ceb6d7190e
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-02-29 12:10:55 +0100

    Fix typo

--- puzzle-showdown/src/main/java/com/codecool/puzzleshowdown/dto/user/UserLeaderBoardDTO.java
+++ puzzle-showdown/src/main/java/com/codecool/puzzleshowdown/dto/user/UserLeaderBoardDTO.java
@@ -1,4 +1,4 @@
 package com.codecool.puzzleshowdown.dto.user;
 
-public record UserLeaderBoardDTO(int rating, String userName, String image) {
+public record UserLeaderBoardDTO(int rating, String username, String image) {
 }
